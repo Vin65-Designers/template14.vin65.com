58cfa5a70abc7ef9377161f877a7bc5e419ca8b3
Kelton Kowalchuk <keltonkowalchuk@gmail.com>
2013-12-12 11:19:24 -0800

    Finish sticky menu and slider fixes for mobile

--- www/assets/js/scripts.js
+++ www/assets/js/scripts.js
@@ -136,7 +136,6 @@
 })(jQuery);
 
 v65.global.init();
-// v65.home.initPhotoGallery();
 v65.page.init();
 
 $('.v65-productGroup-product img').each(function() {
@@ -150,19 +149,125 @@ if($('.homepageProductSlider').length && numOfProducts > 1) {
 	$('.v65-productGroup').flexslider({
 		selector: ".v65-productGroup-products > .v65-productGroup-product",
 		animation: "slide",
-		slideshow: false
+		slideshow: false,
+		useCSS: false
 	});
 }
 
-// $("[v65js=toggleMenu]").click(function(e) {
-// 	if(document.location.hash !== "#mobileMenu"){
-// 		document.location.hash="#mobileMenu";
-// 	} else{
-// 		document.location.hash="";
-// 	}
-// 	return false;
-// });
+$('#mobileMenu').click(function() {
+	$('.mainMenu > ul').stop().slideToggle(500);
+});
 
-function menuToggle() {
-	$('.mainMenu > ul').slideToggle(500);
-}
+// Sticky Plugin
+// =============
+// Author: Anthony Garand
+// Improvements by German M. Bravo (Kronuz) and Ruud Kamphuis (ruudk)
+// Improvements by Leonardo C. Daronco (daronco)
+// Created: 2/14/2011
+// Date: 2/12/2012
+// Website: http://labs.anthonygarand.com/sticky
+// Description: Makes an element on the page stick on the screen as you scroll
+//              It will only set the 'top' and 'position' of your element, you
+//              might need to adjust the width in some cases.
+
+(function($) {
+    var defaults = {
+            topSpacing: 0,
+            bottomSpacing: 0,
+            className: 'is-sticky',
+            wrapperClassName: 'sticky-wrapper'
+        },
+        $window = $(window),
+        $document = $(document),
+        sticked = [],
+        windowHeight = $window.height(),
+        scroller = function() {
+            var scrollTop = $window.scrollTop(),
+                documentHeight = $document.height(),
+                dwh = documentHeight - windowHeight,
+                extra = (scrollTop > dwh) ? dwh - scrollTop : 0;
+            for (var i = 0; i < sticked.length; i++) {
+                var s = sticked[i],
+                    elementTop = s.stickyWrapper.offset().top,
+                    etse = elementTop - s.topSpacing - extra;
+                if (scrollTop <= etse) {
+                    if (s.currentTop !== null) {
+                        s.stickyElement
+                            .css('position', '')
+                            .css('top', '')
+                            .removeClass(s.className);
+                        s.stickyElement.parent().removeClass(s.className);
+                        s.currentTop = null;
+                    }
+                }
+                else {
+                    var newTop = documentHeight - s.stickyElement.outerHeight()
+                        - s.topSpacing - s.bottomSpacing - scrollTop - extra;
+                    if (newTop < 0) {
+                        newTop = newTop + s.topSpacing;
+                    } else {
+                        newTop = s.topSpacing;
+                    }
+                    if (s.currentTop != newTop) {
+                        s.stickyElement
+                            .css('position', 'fixed')
+                            .css('top', newTop)
+                            .addClass(s.className);
+                        s.stickyElement.parent().addClass(s.className);
+                        s.currentTop = newTop;
+                    }
+                }
+            }
+        },
+        resizer = function() {
+            windowHeight = $window.height();
+        },
+        methods = {
+            init: function(options) {
+                var o = $.extend(defaults, options);
+                return this.each(function() {
+                    var stickyElement = $(this);
+
+                    stickyId = stickyElement.attr('id');
+                    wrapper = $('<div></div>')
+                        .attr('id', stickyId + '-sticky-wrapper')
+                        .addClass(o.wrapperClassName);
+                    stickyElement.wrapAll(wrapper);
+                    var stickyWrapper = stickyElement.parent();
+                    stickyWrapper.css('height', stickyElement.outerHeight());
+                    sticked.push({
+                        topSpacing: o.topSpacing,
+                        bottomSpacing: o.bottomSpacing,
+                        stickyElement: stickyElement,
+                        currentTop: null,
+                        stickyWrapper: stickyWrapper,
+                        className: o.className
+                    });
+                });
+            },
+            update: scroller
+        };
+
+    // should be more efficient than using $window.scroll(scroller) and $window.resize(resizer):
+    if (window.addEventListener) {
+        window.addEventListener('scroll', scroller, false);
+        window.addEventListener('resize', resizer, false);
+    } else if (window.attachEvent) {
+        window.attachEvent('onscroll', scroller);
+        window.attachEvent('onresize', resizer);
+    }
+
+    $.fn.sticky = function(method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.sticky');
+        }
+    };
+    $(function() {
+        setTimeout(scroller, 0);
+    });
+})(jQuery);
+$("nav.mainMenu").sticky();
